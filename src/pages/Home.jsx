da2b6e4668adcb849721a13680e98bccfec43eb9
964Code <964code@gmail.com>
2022-01-20 09:45:47 +0100

    Added comments/Todo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import Typewriter from 'typewriter-effect';
 function Home() {
   return (
     <div className='home'>
+      {/* Issues with the page layout. the vh is not looking well and the hero-image eats up the space. */}
       <div className='hero-container'>
         <img
           src='https://images.unsplash.com/photo-1605520321793-d067fdb21c71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
@@ -14,7 +15,8 @@ function Home() {
         />
         <div className='hero-text'>
           <h1 className='hero-title'>
-            <span>I am </span> {/* Add spacing between H! and typewriter */}
+            <span>I am </span>{' '}
+            {/* Add spacing between H! and typewriter. OR remove the H1 and see if there is option through typewriter to have a placeholder text that then uses effect after it*/}
             <Typewriter
               options={{
                 strings: ['Hello', 'World'],
