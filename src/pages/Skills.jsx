2bc29bee68b95a2525375f8d270355fe50876ce1
964Code <964code@gmail.com>
2022-01-21 20:56:09 +0100

    Fixed landing page and began on skills-section

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
 import './Skills.css';
+import { AiFillHtml5 } from 'react-icons/ai';
+import { DiCss3, DiSass } from 'react-icons/di';
+import { SiJavascript } from 'react-icons/si';
+import { FaReact } from 'react-icons/fa';
+import { SiNodedotjs } from 'react-icons/si';
 
 function Skills() {
   document.title = 'Skills';
-  return <div className='skills'></div>;
+  return (
+    <div className='skills'>
+      <div className='skills-text-container'>
+        <h2>Skills & Implementation</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+          architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Delectus sapiente voluptatibus consectetur. Itaque nesciunt adipisci
+          alias possimus ipsum accusantium obcaecati aut vel magni, nam porro
+          laudantium? Ea sequi incidunt laborum! Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Quam obcaecati totam quis dolor
+          consequuntur, minus ipsa. Ex beatae recusandae eligendi!
+        </p>
+      </div>
+      <div className='skill-container'>
+        <div className='skill-list'>
+          <AiFillHtml5 className='skill-icon html' />
+          <p>HTML 5</p>
+        </div>
+        <div className='skill-list'>
+          <DiCss3 className='skill-icon css' />
+          <p>CSS3</p>
+        </div>
+        <div className='skill-list'>
+          <DiSass className='skill-icon sass' />
+          <p>SaSS</p>
+        </div>
+        <div className='skill-list'>
+          <SiJavascript className='skill-icon javascript' />
+          <p>JavaScript</p>
+        </div>
+        <div className='skill-list react'>
+          <FaReact className='skill-icon react' />
+          <p>React</p>
+        </div>
+        <div className='skill-list'>
+          <SiNodedotjs className='skill-icon node' />
+          <p>Node.JS</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
---
+++ src/resources/skills.js
@@ -0,0 +1,37 @@
+const skills = [
+  {
+    id: 1,
+    icon: 'AiFillHtml5',
+    skill: 'HTML5',
+  },
+
+  {
+    id: 2,
+    icon: 'DiCss3',
+    skill: 'CSS3',
+  },
+
+  {
+    id: 3,
+    icon: 'DiSass',
+    skill: 'SaSS',
+  },
+
+  {
+    id: 4,
+    icon: 'SiJavascript',
+    skill: 'JavaScript',
+  },
+
+  {
+    id: 5,
+    icon: 'FaReact',
+    skill: 'React',
+  },
+
+  {
+    id: 6,
+    icon: 'SiNodedotjs',
+    skill: 'Node.JS',
+  },
+];
