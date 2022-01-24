95d8bfc42c1bde6a44ffeea10f218a4c553a9264
964Code <964code@gmail.com>
2022-01-24 14:58:48 +0100

    adjusted skills-section with grid-items and styling

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -9,12 +9,12 @@ import skills from '../resources/skills';
 
 function Skills() {
   const icons = [
-    <AiFillHtml5 className='card-icon' />,
-    <DiCss3 className='card-icon' />,
-    <DiSass className='card-icon' />,
-    <SiJavascript className='card-icon' />,
-    <FaReact className='card-icon' />,
-    <SiNodedotjs className='card-icon' />,
+    <AiFillHtml5 />,
+    <DiCss3 />,
+    <DiSass />,
+    <SiJavascript />,
+    <FaReact />,
+    <SiNodedotjs />,
   ];
 
   return (
@@ -22,46 +22,28 @@ function Skills() {
       <div className='skills-text-container'>
         <h2>Skills & Implementation</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
-          architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Delectus sapiente voluptatibus consectetur. Itaque nesciunt adipisci
-          alias possimus ipsum accusantium obcaecati aut vel magni, nam porro
-          laudantium? Ea sequi incidunt laborum! Lorem ipsum dolor sit, amet
-          consectetur adipisicing elit. Quam obcaecati totam quis dolor
-          consequuntur, minus ipsa. Ex beatae recusandae eligendi!
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum,
+          odit deleniti. Atque alias minus, tenetur obcaecati exercitationem
+          natus cumque illum unde, magni laudantium qui eos minima iusto
+          temporibus, error accusantium! Vel dignissimos ducimus eius, eligendi
+          laborum voluptates, labore voluptatum blanditiis quos rerum facere id
+          ea pariatur minus obcaecati delectus enim! Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Ab quaerat numquam debitis quis neque
+          voluptatum aliquid blanditiis, perferendis qui eos dolorum sed placeat
+          aspernatur, veritatis earum id cum distinctio quisquam.
         </p>
       </div>
 
-      <div className='skill-container'>
+      <div className='grid-container'>
         {skills.map((skill, index) => {
           return (
-            <div className='skill-list card' key={skill.id}>
-              {icons[index]}
+            <div className={`grid-item grid-item-${skill.id}`} key={skill.id}>
+              <div className='card-icon-container'>{icons[index]}</div>
+
               <div className='card-text'>{skill.text}</div>
             </div>
           );
         })}
-
-        {/* <div className='skill-list'>
-          <DiCss3 className='skill-icon css' />
-          <p>CSS3</p>
-        </div>
-        <div className='skill-list'>
-          <DiSass className='skill-icon sass' />
-          <p>SaSS</p>
-        </div>
-        <div className='skill-list'>
-          <SiJavascript className='skill-icon javascript' />
-          <p>JavaScript</p>
-        </div>
-        <div className='skill-list react'>
-          <FaReact className='skill-icon react' />
-          <p>React</p>
-        </div>
-        <div className='skill-list'>
-          <SiNodedotjs className='skill-icon node' />
-          <p>Node.JS</p>
-        </div> */}
       </div>
     </div>
   );
--- src/resources/skills.js
+++ src/resources/skills.js
@@ -3,42 +3,42 @@ const skills = [
     id: 1,
     icon: 'AiFillHtml5',
     skill: 'HTML5',
-    text: 'HTML5 implementation',
+    text: 'HTML5',
   },
 
   {
     id: 2,
     icon: 'DiCss3',
     skill: 'CSS3',
-    text: 'CSS3 implementation, animation and much more',
+    text: 'CSS3',
   },
 
   {
     id: 3,
     icon: 'DiSass',
     skill: 'SaSS',
-    text: 'SaSS implementation for cleaner and easier to read CSS-code',
+    text: 'SaSS',
   },
 
   {
     id: 4,
     icon: 'SiJavascript',
     skill: 'JavaScript',
-    text: 'JavaScript implementation, DOM-maniplulation.',
+    text: 'JavaScript',
   },
 
   {
     id: 5,
     icon: 'FaReact',
     skill: 'React',
-    text: 'React implementation. Able to build application with React',
+    text: 'React',
   },
 
   {
     id: 6,
     icon: 'SiNodedotjs',
     skill: 'Node.JS',
-    text: 'Node Back-end implementation for different usages',
+    text: 'Node Back-end',
   },
 ];
 
