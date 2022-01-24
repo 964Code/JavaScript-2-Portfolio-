6fa40e88d985e6e8b7846bebb22be4d47ffec544
964Code <964code@gmail.com>
2022-01-24 21:51:19 +0100

    added icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import './Footer.css';
+import {
+  AiFillTwitterCircle,
+  AiFillGithub,
+  AiFillLinkedin,
+} from 'react-icons/ai';
+import BaldIcon from '../resources/baldIconTest.svg';
 
 function Footer() {
-  return <div className='footer'></div>;
+  return (
+    <div className='footer'>
+      <div className='footer-icon-container'>
+        <a href='http://'>
+          <AiFillTwitterCircle />
+        </a>
+
+        <a href='http://'>
+          <AiFillGithub />
+        </a>
+
+        <a href='http://'>
+          <AiFillLinkedin />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
