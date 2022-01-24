75907520978b747b816e6027bd193b8a4b7009c3
964Code <964code@gmail.com>
2022-01-24 16:01:44 +0100

    added contact-page form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,33 @@ import React from 'react';
 import './Contact.css';
 
 function Contact() {
-  return <div className='contact' id='contact'></div>;
+  return (
+    <div className='contact' id='contact'>
+      <div className='container'>
+        <div className='contact-box'>
+          <div className='left'></div>
+          <div className='right'>
+            <h2>Contact Me</h2>
+            <input
+              type='text'
+              className='field'
+              placeholder='Your Name'
+              required
+            />
+            <input
+              type='email'
+              className='field'
+              placeholder='Your Email'
+              required
+            />
+            <input type='text' className='field' placeholder='Phone' />
+            <textarea placeholder='Message' className='field'></textarea>
+            <button className='btn'>Send</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
